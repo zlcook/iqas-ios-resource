452eb5ae5463f543a564da93005c444cf2b265e9
zhougit <594389970@qq.com>
2017-03-22 10:18:52 +0800

    first commit

---
+++ src/main/java/com/zlcook/iqas/IqasIosResourceApplication.java
@@ -0,0 +1,12 @@
+package com.zlcook.iqas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IqasIosResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IqasIosResourceApplication.class, args);
+	}
+}
